6a5c5303dfa691f65142799674cee24f108ee911
PriyanshuNaredi <en20cs301324@medicaps.ac.in>
2024-06-21 13:26:11 +0530

    README.md Section-14/intermediate.js

--- Section-14/intermediate.js
+++ Section-14/intermediate.js
@@ -1,4 +1,22 @@
-// Random No 
+//                                            Random No 
 var n = Math.random(); // Lies from 0 to 0.999
 
+//                                             If Else
+if (1<2) {
+    console.log("hello");
+} else {
+    console.log("Nope");
+}
+
+//                                           "===" V/S "=="
+var a = 1; // number
+var b = '1'; // string
+
+if (a == b){
+    console.log('simple check for data');
+}
+
+if(a === b){
+    console.log('check for data type also');
+}
 
