9c21f813878b10612f133a8c8e46e60ae8d4f4c5
PriyanshuNaredi <en20cs301324@medicaps.ac.in>
2024-06-25 12:32:46 +0530

    intermediate js

--- Section-14/intermediate.js
+++ Section-14/intermediate.js
@@ -39,21 +39,52 @@ function isLeap(year) {
 
 //                                                 Arrays
 
-var arr = [1,2,3,4]
+var arr = [1, 2, 3, 4]
 console.log(arr[2]);
 arr.includes(3);
 
 
+//                                                While Loop
 
+var i = 0;
+while (i < 3) {
+    console.log('current value of i is ' + i);
+    i++;
+}
 
+var i = 100;
+while (i >= 0) {
+    var l = i - 1;
+    if (i <= 0) {
+        console.log('No more bottles of beer on the wall, no more bottles of beer. Go to store and buy some more, 99 bottles of beer on the wall');
+    } else
+        console.log(i + ' bottles of beer on the wall, ' + i + ' bottles of beer. Take 1 down, pass it around, ' + l + ' bottles of beer on the wall.');
+    i--;
+}
 
+//                                                 For Loop
+for (let i = 0; i < 3; i++) {
+    console.log(i);
+}
 
-
-
-
-
-
-
+function fibonacciGenerator(n) {
+    var output = [];
+    if (n === 1) {
+        output = [0];
+    }
+    else if (n === 2) {
+        output = [0, 1];
+    }
+    else {
+        output = [0, 1];
+        for (var i = 2; i < n; i++) {
+            output.push(output[output.length - 2] + output[output.length - 1]);
+        }
+    }
+    return output;
+}
+output = fibonacciGenerator(5);
+console.log(output)
 
 
 
