accd6c44067906c890f2386d2f4c8a0e59d25526
PriyanshuNaredi <en20cs301324@medicaps.ac.in>
2024-06-25 11:50:31 +0530

    intermediate js

--- Section-14/intermediate.js
+++ Section-14/intermediate.js
@@ -2,7 +2,7 @@
 var n = Math.random(); // Lies from 0 to 0.999
 
 //                                             If Else
-if (1<2) {
+if (1 < 2) {
     console.log("hello");
 } else {
     console.log("Nope");
@@ -12,11 +12,84 @@ if (1<2) {
 var a = 1; // number
 var b = '1'; // string
 
-if (a == b){
+if (a == b) {
     console.log('simple check for data');
 }
 
-if(a === b){
+if (a === b) {
     console.log('check for data type also');
 }
+
+function isLeap(year) {
+    if (year % 4 === 0) {
+        if (year % 100 === 0) {
+            if (year % 400 === 0) {
+                return "Leap year.";
+            } else {
+                return "Not leap year.";
+            }
+        } else {
+            return "Leap year.";
+        }
+    } else {
+        return "Not leap year.";
+    }
+}
+
+
+//                                                 Arrays
+
+var arr = [1,2,3,4]
+console.log(arr[2]);
+arr.includes(3);
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
