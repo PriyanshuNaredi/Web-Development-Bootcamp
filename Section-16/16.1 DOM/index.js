92de918195b9c6925ec42b2d35339cafc89cbc2a
PriyanshuNaredi <en20cs301324@medicaps.ac.in>
2024-06-27 12:54:49 +0530

    More DOM

--- Section-16/16.1 DOM/index.js
+++ Section-16/16.1 DOM/index.js
@@ -52,3 +52,32 @@ document.querySelector('li a').style.color = 'purple';
 document.querySelectorAll('ul .list')
 /* Output ->
 NodeList(3) [li.list, li.list, li.list] */
+
+
+document.querySelector('button').classList
+/* return the class name assigned to this selector */
+document.querySelector('button').classList.add('new-class-name')
+/* adds new class name to this selector */
+document.querySelector('button').classList.toggle('new-class-name')
+/* turn on & off the class from the selector */
+
+
+document.querySelector("h1").innerHTML;
+/* output -> "<strong>Hello</strong>"
+returns the HTML present inside the selector (text, other html tags, etc)
+*/
+document.querySelector("h1").textContent;
+/*  "Hello" 
+return only text contain
+*/
+document.querySelector("h1").innerHTML = "<em>Good Bye</em>";
+/* "<em>Good Bye</em>" */
+
+
+document.querySelector("a");
+//<a href="https://www.google.com">Google</a>
+document.querySelector("a") .attributes;
+// NamedNodeMap {0: href, href: href, length: 1}
+document.querySelector("a").getAttribute("href");
+// "https://www.google.com"
+document.querySelector("a").setAttribute("href", "https://www.bing.com");
