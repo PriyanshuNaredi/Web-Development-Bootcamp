0cac2f72c6c59eea0cdecc53d6bb1a715aeaaa6c
PriyanshuNaredi <en20cs301324@medicaps.ac.in>
2024-07-06 21:20:29 +0530

    QR Code Generator

---
+++ Section-21/2.3 NPM/index.js
@@ -0,0 +1,15 @@
+// var generateName = require('sillyname');
+
+import generateName from "sillyname"
+var sillyName = generateName();
+
+console.log(`My Name is ${sillyName}`);
+// console.log("My Name is ${sillyName}"); // Output -> My Name is ${sillyName}
+
+import superheroes from "superheroes";
+
+const name = superheroes.random();
+console.log(`I am ${name}!`);
+
+
+
