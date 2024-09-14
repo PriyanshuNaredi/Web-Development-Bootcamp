d4e26e0912f0938e3ab78debec018bb1d73a7192
Priyanshu Naredi <naredipriyanshu29@gmail.com>
2024-09-14 07:36:19 -0700

    Middleware to index.js

--- Section-22/22.4 Middleware/index.js
+++ Section-22/22.4 Middleware/index.js
@@ -8,7 +8,7 @@ const __dirname = dirname(fileURLToPath(import.meta.url));
 const app = express();
 const port = 3000;
 
-app.use(express.urlencoded({ extended: true }));
+app.use(bodyParser.urlencoded({ extended: true }));
 
 app.get("/", (req, res) => {
   console.log(__dirname + "/public/index.html");
