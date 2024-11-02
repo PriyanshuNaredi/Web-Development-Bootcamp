18e4899abf87059d4cda52514ae45e1629b74eb0
Priyanshu Naredi <naredipriyanshu29@gmail.com>
2024-11-02 09:32:08 -0400

    Todo List Item

--- Section-30/Postgres/index.js
+++ Section-30/Postgres/index.js
@@ -1,8 +1,19 @@
 import express from "express";
 import bodyParser from "body-parser";
+import pg from "pg";
 
 const app = express();
 const port = 3000;
+
+const db = new pg.Client({
+  user: "postgres",
+  host: "localhost",
+  database: "permalist",
+  password: "1234",
+  port: 5432  
+});
+
+db.connect();
 
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(express.static("public"));
@@ -12,22 +23,64 @@ let items = [
   { id: 2, title: "Finish homework" },
 ];
 
-app.get("/", (req, res) => {
+async function todoListItems() {
+  const result = await db.query(
+    "SELECT * FROM items;"
+  );
+  console.log(result.rows);
+  items = result.rows;
+}
+
+app.get("/", async(req, res) => {
+  const a = await todoListItems();
   res.render("index.ejs", {
     listTitle: "Today",
     listItems: items,
   });
 });
 
-app.post("/add", (req, res) => {
+app.post("/add", async(req, res) => {
   const item = req.body.newItem;
   items.push({ title: item });
+  console.log(item);
+  try {
+    await db.query(
+      "INSERT INTO items (title) VALUES ($1);",
+      [item]
+    );
+  } catch (err) {
+    console.log(err);
+  }
   res.redirect("/");
 });
 
-app.post("/edit", (req, res) => {});
+app.post("/edit", async(req, res) => {
+  const newTodoItem = req.body.updatedItemTitle;
+  const id = req.body.updatedItemId;
+  
+  try {
+    await db.query(
+      "UPDATE items SET title = ($1) WHERE id = ($2)",
+      [newTodoItem,id]
+    );
+  } catch (error) {
+    console.log(err);
+  }
+  res.redirect("/");
+});
 
-app.post("/delete", (req, res) => {});
+app.post("/delete", async(req, res) => {
+  const deleteItem = req.body.deleteItemId;
+  try {
+    await db.query(
+      "DELETE FROM items WHERE id = ($1);",
+      [deleteItem]
+    );
+  } catch (err) {
+    console.log(err);
+  }
+  res.redirect("/");
+});
 
 app.listen(port, () => {
   console.log(`Server running on port ${port}`);
--- Section-30/Postgres/queries.sql
+++ Section-30/Postgres/queries.sql
@@ -1,3 +1,4 @@
+-- Active: 1730209103320@@127.0.0.1@5432@permalist@public
 CREATE TABLE items (
   id SERIAL PRIMARY KEY,
   title VARCHAR(100) NOT NULL
